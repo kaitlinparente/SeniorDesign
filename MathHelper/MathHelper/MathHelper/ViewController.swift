07100f87053de278021b4910662abcae87e51c82
Antonio Ripepe <47901021+AntonioRipepe@users.noreply.github.com>
2019-07-02 20:19:32 -0400

    Add files via upload

---
+++ MathHelper/MathHelper/MathHelper/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  MathHelper
+//
+//  Created by Antonio Ripepe on 7/2/19.
+//  Copyright © 2019 Antonio Ripepe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sideMenus()
+        customizeNavBar()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func sideMenus() {
+        if revealViewController() != nil {
+            menuButton.target = revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            revealViewController().rearViewRevealWidth = 275
+            revealViewController().rightViewRevealWidth = 160
+            
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+
+    func customizeNavBar() {
+        navigationController?.navigationBar.barTintColor = UIColor(displayP3Red: 255/255, green: 255/255, blue: 0/255, alpha: 1)
+        navigationController?.navigationBar.barTintColor = UIColor(displayP3Red: 247/255, green: 137/255, blue: 27/255, alpha: 1)
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+    }
+
+}
