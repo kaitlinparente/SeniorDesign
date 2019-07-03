6ae9611d249564abaa56080243d4fd6f1dd073bb
Kaitlin Parente <kaitlinparente17@gmail.com>
2019-07-02 21:20:53 -0400

    why isn't this working

--- MathHelper/MathHelper/MathHelper/SWRevealViewController.h
+++ MathHelper/MathHelper/MathHelper/SWRevealViewController.h
@@ -1,5 +1,7 @@
 /*
 testing commit
+ why isnt this working 
+ 
  Copyright (c) 2013 Joan Lluch <joan.lluch@sweetwilliamsl.com>
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
